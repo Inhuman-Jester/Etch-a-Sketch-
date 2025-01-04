1e9fc9d8d699001bc3583400eb21de00d2ac5885
Inhuman <hj29official@gmail.com>
2025-01-04 20:02:35 +0530

    bit of regulation changes, aded a write button, made the code cleaner

--- Hohoho/etch-a-sketch/script.js
+++ Hohoho/etch-a-sketch/script.js
@@ -1,11 +1,14 @@
-
-const container=document.querySelector(".container")
-
+//select container
+const container=document.querySelector(".container");
 
 //set no. of grids dynamically
-const gridSize=document.querySelector(".gridSize")
+const gridSize=document.querySelector(".gridSize");
+
 gridSize.addEventListener("click", (event)=>{
-    let size=prompt("Enter the pixel size from 0-100")
+    let size=0;
+    while(size<1 || size>100){
+        size=prompt("Enter the pixel size from 0-100");
+    }
     run(size);
 })
 
@@ -14,55 +17,68 @@ function run(size){
 
     //clear all of the divs inside to reset the whole board and give board with new dimensions
     container.textContent=""
+
     //16X16 grid
     for(let i=0; i<size*size; i++){
         const element=document.createElement("div");
-        element.classList.add("cell")
+        element.classList.add("cell");
         container.appendChild(element);
     }
 
     //Select all cells
-    const cells=document.querySelectorAll(".cell")
+    const cells=document.querySelectorAll(".cell");
 
     //dynamically allocating no. of grids
     cells.forEach(element=>{
-        element.style.flexBasis=`calc(100% / ${size})`
-    })
-
-    //set up a clear button
-    const clear= document.querySelector(".clear")
-    clear.addEventListener("click", (event)=>{
-        cells.forEach(element=>{
-            element.style.backgroundColor="white";
-        })
+        element.style.flexBasis=`calc(100% / ${size})`;
     })
 
     //change colour
     container.addEventListener("mouseover", (event)=>{
         if(event.target.classList.contains("cell")){
 
-            if(event.target.classList.contains("active"))
-                event.target.style.backgroundColor="black";
+            if(event.target.classList.contains("active")){
+                if(event.target.classList.contains("write"))
+                    event.target.style.backgroundColor="black";
 
-            if(event.target.classList.contains("erase"))
-                event.target.style.backgroundColor="white";
+                if(event.target.classList.contains("erase"))
+                    event.target.style.backgroundColor="white";
+            }
         }
     })
 
-    //Activating drawing board using space key
+    //Activating drawing board using enter key
     document.addEventListener("keydown",(event)=>{
-        if(event.key==" "){
+        if(event.key=="Enter"){
             cells.forEach(element=>{
-                element.classList.toggle("active")
+                element.classList.toggle("active");
             })
         }
     })
 
-    //button to erase
-    eraser=document.querySelector(".eraser")
-    eraser.addEventListener("click", (event)=>{
-        cells.forEach(element=>{
-            element.classList.toggle("erase");
-        })
+    //select buttons to apply features
+    const btn=document.querySelector(".btn");
+    btn.addEventListener("click", event=>{
+
+        //add functionality to write button
+        if(event.target.className==="write"){
+            cells.forEach(element=>{
+                element.classList.toggle("write");
+            })
+        }
+
+        //add functionality to erase button
+        if(event.target.className==="erase"){
+            cells.forEach(element=>{
+                element.classList.toggle("erase");
+            })
+        }
+
+        //add functionality to clear button
+        if(event.target.className==="clear"){
+            cells.forEach(element=>{
+                element.style.backgroundColor="white";
+            })
+        }
     })
 }
